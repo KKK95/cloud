214b4ad470e843335522818d06afd29436d7d6ad
KKK95 <mkyang95@gmail.com>
2016-11-13 17:10:08 +0800

    Add files via upload

--- meeting_cloud/back_end/group/build_group.php
+++ meeting_cloud/back_end/group/build_group.php
@@ -11,7 +11,11 @@
 	
 	$datetime = date("Y-m-d H:i:s");
 	
-	$sql = "insert INTO group_leader (leader_id, group_name, date_time) 
+	echo $_SESSION['id'];
+	echo $_POST['group_name'];
+	
+	
+	$sql = "insert INTO group_leader (member_id, group_name, date_time) 
 			VALUES ('".$_SESSION['id']."', '".$_POST['group_name']."', '".$datetime."')";
 	
 	if($result = $conn->query($sql))
@@ -21,7 +25,7 @@
 		echo "build group failed\n";	
 	}
 	
-	$sql = "select group_id from group_leader where date_time = '".$datetime."' and leader_id = '".$_SESSION['id']."'";			//要取得group id
+	$sql = "select group_id from group_leader where date_time = '".$datetime."' and member_id = '".$_SESSION['id']."'";			//要取得group id
 	
 	$result = $conn->query($sql);
 	
@@ -42,20 +46,30 @@
 		} 
 		else if ($start >= 2)
 		{
+			
 			$post[$varname] = $varvalue;
 			$sql = "insert INTO group_member (group_id, member_id) 
 											VALUES ('".$group_id."','"
-													  .$varvalue."')";
+													  .$post[$varname]."')";
+			$conn->query($sql);
 		}
 		$start = $start + 1;
 	}
+	
 	
 	//刪除一些不在member 列表中的 member id
 	$sql = "delete from group_member		
 			where group_id = '".$row['group_id']."
 			member_id not in (	select id from member	)";
 	$conn->query($sql);
-
-	header("Location: ../../device/employee/group/group.php?group_id=".$row['group_id']);
-
+	
+	if ($_SESSION['platform'] == "device")
+	{
+		header("Location: ../../device/employee/group/group.php?group_id=".$row['group_id']);
+	}	
+	else if ($_SESSION['platform'] == "web")
+	{
+		header("Location: ../../web/employee_web/group/group.php?group_id=".$row['group_id']);
+	}	
+	
 ?>
